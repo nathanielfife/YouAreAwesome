4ba732c6632762d14f657d88b9d530c6ef989f72
Travis Fife <travisthefife@gmail.com>
2025-08-12 17:05:23 -0400

    Added while loops and repeat while loops and made images and text not repeat

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
+    @State private var lastImageNumber : Int = -1
+    @State private var lastMessageNumber : Int = -1 // last messageNuber will never be -1
     var body: some View {
         
         VStack {
@@ -30,13 +32,26 @@ struct ContentView: View {
                 .shadow(radius: 50)
                 .animation(.default, value: imageName)
             
-         
+            
             Spacer()
             
             Button("Show Message") {
                 let messageList = ["Never let go of the goodness!" , "Adda Boy!", "You Are Awesome!" , "Don't forget that your amazing!", "Good job!" , "There is always a reason to smile!", "You are a child of God!" , "You are terrific!"]
-                message = messageList[Int.random(in: 0...messageList.count - 1)]
-                imageName = "image\(Int.random(in: 0...9))"
+                //message = messageList[Int.random(in: 0...messageList.count - 1)]
+                //                imageName = "image\(Int.random(in: 0...9))"
+                var messageNumber: Int
+                repeat {
+                    messageNumber = Int.random(in: 0...messageList.count - 1)
+                } while messageNumber == lastMessageNumber
+                message = messageList[messageNumber]
+                lastMessageNumber = messageNumber
+                
+                var imageNumber: Int
+               repeat {
+                    imageNumber = Int.random(in: 0...9)
+                } while imageNumber == lastImageNumber
+                imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
